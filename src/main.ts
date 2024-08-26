2febfaf1a7c4059f03a9a8127a9df2a76d9686c0
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-08-27 01:21:42 +0800

    ddd

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger/api', app, document);
-  await app.listen(5000);
+  await app.listen(9000);
 }
 bootstrap();
